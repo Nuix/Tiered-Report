8fc3e0d8ad6c18d78d3f74c0b2f661cc999e272c
Jason Wells <jason.wells@nuix.com>
2018-09-13 17:26:27 -0700

    Added year month aspects which support reporting rows with no items
    - ItemDateYearMonthWithZerosAspect
    - TopLevelItemDateYearMonthWithZerosAspect
    - Made supporting modifications to other classes.

--- Java/src/main/java/com/nuix/tieredreport/aspects/ItemAspect.java
+++ Java/src/main/java/com/nuix/tieredreport/aspects/ItemAspect.java
@@ -22,8 +22,10 @@ public interface ItemAspect {
 	public String getAspectName();
 	public String getAspectReportLabel();
 	public boolean isPerItem();
+	public boolean reportsZeroItems();
 	public void recordItemValues(Case nuixCase, Utilities utilities, Map<Object,RoaringBitmap> aspectBitmaps, TieredReportData data, Item item, ItemInfo itemInfo);
 	public void setup(Case nuixCase, Utilities utilities);
+	public void postProcess(Map<Object,RoaringBitmap> aspectBitmaps, TieredReportData data);
 	public void cleanup();
 	public void recordValues(Case nuixCase, Utilities utilities, Map<Object,RoaringBitmap> aspectBitmaps, TieredReportData data, Collection<Item> inputItems);
 	public List<Object> customizeValueSort(Collection<Object> values);
